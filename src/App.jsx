ed4b0281bc8b8471466b2e805377978ba5414ed3
Manik Hassan <zabairzubair@gmail.com>
2023-09-11 21:10:55 +0600

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,24 @@
 // eslint-disable-next-line no-unused-vars
 import React from "react";
-
 import Header from "./components/Header";
 import Banner from "./components/Banner";
-import Nav from "./components/Nav";
-import About from "./components/About";
-import Services from "./components/Services";
-import Work from "./components/Work";
-import Contact from "./components/Contact"
+import Experts from "./components/Experts";
+import NewsLetter from "./components/NewsLetter";
+import Plans from "./components/Plans";
+import Footer from "./components/Footer";
+
+
+
 const App = () => {
   return (
-<div className="bg-gradient-to-r from-cyan-500 via-green-400 to-violet-400">
+<div >
 <Header />
 <Banner />
-<Nav />
-<About />
-<Services />
-<Work />
-<Contact />
-<div className="h-[4000px]"></div>
+<Experts />
+<NewsLetter />
+<Plans />
+<Footer />
+
 </div>
   )
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,28 +1,63 @@
+
 // eslint-disable-next-line no-unused-vars
 import React from 'react';
-import Logo from '../img/windo.jpg'
+import {AiOutlineMenu, AiOutlineClose} from 'react-icons/ai';
+import { useState } from 'react';
 
 const Header = () => {
+  const [toggle, setToggle]=useState(false);
   return (
-    <header className='h-[15px]'>
+    <div className='bg-[#2648fb] p-5'>
+        <div className='max-w-[1240px]  mx-auto items-center flex justify-between'>
+        <div className='text-3xl font-bold font-manik tracking-wider text-gradient'>
+            AramCo
+            </div>
 
-      <div className='mb-[-82px]'>
-        <div className='flex justify-between'>
-          {/* logo */}
-          {/* <a href='#'>
-          <h2 className='bg-clip-text text-transparent bg-gradient-to-r from-pink-500 to-violet-500 text-[1rem] font-extrabold'>Manik <span>Hasan</span> </h2>
+      {
+        toggle ?
+
+        <AiOutlineMenu onClick={() => setToggle(!toggle)} className='text-2xl md:hidden block' />
+        :
+         <AiOutlineClose onClick={() => setToggle(!toggle)}  className='text-2xl md:hidden block' />
+      }
+  {/* Responsive menu start*/}
+      {
+        toggle ?
+
+        
+          <ul className='md:hidden  fixed bg-black/80 left-[0] top-[76px] text-white w-[50%] text-center duration-700 font-manik'
+            >
+                <li className='p-5'>Home</li>
+                <li className='p-5'>Company</li>
+                <li className='p-5' >Resources</li>
+                <li className='p-5' >About</li>
+                <li className='p-5'>Contact</li>
+                
+            </ul>
+          :
+           <ul className='md:hidden  fixed bg-black/80  top-[76px] text-white w-[50%] text-center duration-700 left-[-100%]'
+            >
+                <li className='p-5'>Home</li>
+                <li className='p-5'>Company</li>
+                <li className='p-5' >Resources</li>
+                <li className='p-5' >About</li>
+                <li className='p-5'>Contact</li>
+                
+            </ul>
+      } 
+         {/* Responsive menu end*/}
+
+           <ul className='hidden md:flex text-white gap-10'>
+                <li className='p-5'>Home</li>
+                <li className='p-5'>Company</li>
+                <li className='p-5' >Resources</li>
+                <li className='p-5' >About</li>
+                <li className='p-5'>Contact</li>
+                
+            </ul>
           
-          </a> */}
-          <div className=' '>
-          <img className='w-[3rem] h-[3rem] m-2 rounded-full' src={Logo} alt='' />
-          </div>
-                    <div className='mt-1'>
-               <button className='btnn mr-1'>Work with me</button>
-          </div>
-       
         </div>
-      </div>
-    </header>
+    </div>
   )
 }
 
