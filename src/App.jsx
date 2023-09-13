8a5aaeec8aae8f44169863062da458f6bb9f9a5a
Manik Hassan <zabairzubair@gmail.com>
2023-09-13 06:40:02 +0600

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,12 @@
 // eslint-disable-next-line no-unused-vars
 import React from "react";
-import Header from "./components/Header";
-import Banner from "./components/Banner";
-import Experts from "./components/Experts";
-import NewsLetter from "./components/NewsLetter";
-import Plans from "./components/Plans";
-import Footer from "./components/Footer";
 
 
 
 const App = () => {
   return (
 <div >
-<Header />
-<Banner />
-<Experts />
-<NewsLetter />
-<Plans />
-<Footer />
+<h2 className="text-black">Manik Hasan</h2>
 
 </div>
   )
